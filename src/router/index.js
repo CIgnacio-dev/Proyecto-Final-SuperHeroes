a1578fdf3c4dcdadf3ba6a922480f94cb6c373db
Carlos <60223426+CIgnacio-dev@users.noreply.github.com>
2020-06-26 16:58:22 -0400

    Files
    Files Proyecto Team CaGe

---
+++ src/router/index.js
@@ -0,0 +1,42 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+
+Vue.use(VueRouter)
+
+  const routes = [
+  {
+    path: '/Home',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/Login',
+    name: 'Login',
+    component: () => import('../views/Login.vue')
+  },
+  {
+    path: '/Batalla',
+    name: 'Batalla',
+    component: () => import('../views/Batalla.vue')
+  },
+  {
+    path: '/Favoritos',
+    name: 'Favoritos',
+    component: () => import('../views/Favoritos.vue')
+  },
+  {
+    path: '/Historia',
+    name: 'Historia',
+    component: () => import('../views/Historia.vue')
+  }
+  
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
---
+++ src/views/Home.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="home">
+<div>
+  <b-carousel
+    id="carousel-fade"
+    style="text-shadow: 0px 0px 2px #000"
+    fade
+    indicators
+    img-width="1024"
+    img-height="480"
+  >
+    <b-carousel-slide
+      caption="First slide"
+      img-src="https://picsum.photos/1024/480/?image=10"
+    ></b-carousel-slide>
+    <b-carousel-slide
+      caption="Second Slide"
+      img-src="https://picsum.photos/1024/480/?image=12"
+    ></b-carousel-slide>
+    <b-carousel-slide
+      caption="Third Slide"
+      img-src="https://picsum.photos/1024/480/?image=22"
+    ></b-carousel-slide>
+  </b-carousel>
+</div>
+
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+ <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+  
+}
+</script>
